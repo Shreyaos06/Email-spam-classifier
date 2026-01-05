18f7f3003d4de9ef6beb99f437cb4f76749eaa5c
Shreyaos06 <shreyaos2k6@gmail.com>
2026-01-05 16:07:59 +0530

    Delete Email_spam_detector directory

--- Email_spam_detector/preprocess.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def clean_text(text):
-    if not text:
-        return []   # for empty string the function returns an empty list
-
-    text = text.lower()
-    text = re.sub(r'[^a-z\s]', '', text) #removes all characters except lowercase letters and spaces
-    return text.split()  #returns the token list
